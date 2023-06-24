6e878ca934e623ea17d9e247a40992b7ab817792
noelkawm <60691371+noelkawm@users.noreply.github.com>
2023-06-24 22:06:42 +0100

    Update LSTM_ADASYN.py

--- LSTM_ADASYN.py
+++ LSTM_ADASYN.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
 import os
 import numpy as np
@@ -35,9 +29,6 @@ import argparse
 import logging
 
 
-# In[ ]:
-
-
 logging.basicConfig(filename="logLSTM_ADASYN",
                     filemode='a',
                     format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
@@ -47,10 +38,6 @@ logging.basicConfig(filename="logLSTM_ADASYN",
 logging.info("Running LSTM_ADASYN.py")
 
 logger = logging.getLogger('LSTM_ADASYN')
-
-
-# In[ ]:
-
 
 """scope = ['https://www.googleapis.com/auth/drive']
 service_account_json_key = 'my_key.json'
@@ -112,24 +99,12 @@ logger.info('Loaded dataset from Google Drive')
     
 """
 
-
-# In[2]:
-
-
 df = pd.read_csv("combined_data.csv", index_col=0)
 logger.info('Loaded dataset')
 
 
-# In[3]:
-
-
 y = df['Type']
 X = df.loc[:, df.columns != "Type"]
-#print(y)
-#print(X)
-
-
-# In[4]:
 
 
 su = ADASYN(random_state=42)
@@ -137,21 +112,10 @@ X_su, y_su = su.fit_resample(X, y)
 logger.info('Dataset is balanced')
 
 
-# In[5]:
-
-
 X_su = X_su.to_numpy()
 y_su = y_su.to_numpy()
 
-
-# In[6]:
-
-
 #X_train, X_test, y_train, y_test = train_test_split(X_su, y_su, test_size = 0.25, random_state=0)
-
-
-# In[7]:
-
 
 model = keras.Sequential()
 
@@ -171,9 +135,6 @@ model.add(layers.Dense(3, kernel_initializer='normal',activation='softmax'))
 #model.summary()
 
 
-# In[ ]:
-
-
 model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
     filepath= 'modelLSTM_ADASYN.{epoch:02d}-{accuracy:.4f}-{val_accuracy:.4f}.h5',
     save_weights_only=True,
@@ -182,14 +143,8 @@ model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
     save_best_only=True)
 
 
-# In[8]:
-
-
 model.compile(loss='categorical_crossentropy',optimizer =tf.keras.optimizers.Adam(learning_rate=0.001),metrics=['accuracy'])
 logger.info('Model is compiled')
-
-
-# In[9]:
 
 
 y_train_new = np.zeros((y_su.shape[0],3))
@@ -197,14 +152,8 @@ for i in range(y_su.shape[0]):
     y_train_new[i][y_su[i]] = 1
 
 
-# In[11]:
-
-
 history = model.fit(X_su,y_train_new,epochs=5,validation_split=0.05,verbose=0, callbacks = [model_checkpoint_callback])
 logger.info('Model is trained')
-
-
-# In[ ]:
 
 
 plt.plot(history.history['accuracy'])
@@ -216,9 +165,6 @@ plt.legend(['train', 'val'], loc='upper left')
 plt.show()
 
 
-# In[ ]:
-
-
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
 plt.title('model loss')
@@ -228,16 +174,10 @@ plt.legend(['train', 'val'], loc='upper left')
 plt.show()
 
 
-# In[ ]:
-
-
 y_pred = model.predict(X_su)
 y_pred_new = np.zeros(y_pred.shape[0])
 for i in range(y_pred.shape[0]):
     y_pred_new[i] = np.argmax(y_pred[i])
-
-
-# In[ ]:
 
 
 matrix = confusion_matrix(y_su, y_pred_new)
@@ -252,4 +192,3 @@ plt.xlabel('Predictions', fontsize=18)
 plt.ylabel('Actuals', fontsize=18)
 plt.title('Confusion Matrix', fontsize=18)
 plt.show()
-
