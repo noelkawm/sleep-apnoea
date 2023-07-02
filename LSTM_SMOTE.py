5dcd2b6f49f72a9c7bb40bb204ab45c42d434d80
OliverFaust <oliver.faust@gmail.com>
2023-07-02 22:15:35 +0100

    New LSTM

--- LSTM_SMOTE.py
+++ LSTM_SMOTE.py
@@ -1,10 +1,11 @@
+import sys, getopt
 import pandas as pd
 import os
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-from scipy.signal import resample
+#from scipy.signal import resample
 from imblearn.under_sampling import NearMiss
 from imblearn.over_sampling import BorderlineSMOTE
 from imblearn.over_sampling import ADASYN
@@ -27,196 +28,176 @@ import matplotlib.pyplot as plt
 import time
 import argparse
 import logging
+import socket
+timeout_in_sec = 60*60*10 # 10h timeout limit
+socket.setdefaulttimeout(timeout_in_sec)
 
+def driveUpload(service, filepath):
+    body = {'name': filepath, 'mimeType': 'application/octet-stream', 'parents': ['1qLxroprD2jwb1ag6vUwYsdCM2imWH3Cx']}
+    media = MediaFileUpload(filepath, mimetype = 'text/html')
+    fiahl = service.files().create(body=body, media_body=media).execute()
+    logging.info("Created file '%s' id '%s'." % (fiahl.get('name'), fiahl.get('id')))
 
-logging.basicConfig(filename="logLSTM_SMOTE",
-                    filemode='a',
-                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
-                    datefmt='%H:%M:%S',
-                    level=logging.INFO)
+def createModel(epochs, batch_size, noRows):
 
-logging.info("Running LSTM_SMOTE.py")
-
-logger = logging.getLogger('LSTM_SMOTE')
-
-"""scope = ['https://www.googleapis.com/auth/drive']
-service_account_json_key = 'my_key.json'
-credentials = service_account.Credentials.from_service_account_file(
-                              filename=service_account_json_key, 
-                              scopes=scope)
-service = build('drive', 'v3', credentials=credentials)
-
-logger.info('Connected to Google Drive')
-
-df = pd.DataFrame()
-
-page_token = None
-items=[]
-
-while True:
-    results = service.files().list(pageSize=1000, fields="nextPageToken, files(id, name)", q='name contains "csv"', pageToken=page_token).execute()
-    # get the results
-    items.extend(results.get('files', []))
-    page_token = results.get("nextPageToken", None)
-    if page_token is None:
-        break
+    logging.basicConfig(filename="logLSTM_SMOTE",
+                        filemode='a',
+                        format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                        datefmt='%H:%M:%S',
+                        level=logging.INFO)
+    
+    logging.info("Running LSTM_SMOTE.py")
+    
+    scope = ['https://www.googleapis.com/auth/drive']
+    service_account_json_key = 'my_key.json'
+    credentials = service_account.Credentials.from_service_account_file(
+                                  filename=service_account_json_key, 
+                                  scopes=scope)
+    service = build('drive', 'v3', credentials=credentials)
+    logging.info('Connected to Google Drive')
+    
+    logger = logging.getLogger('LSTM_SMOTE')
+    
+    df = pd.read_csv("combined_data.csv", index_col=0,nrows=noRows)
+    logger.info('Loaded dataset')
+    
+    y = df['Type']
+    X = df.loc[:, df.columns != "Type"]
+    
+    su = BorderlineSMOTE(random_state=42)
+    X_su, y_su = su.fit_resample(X, y)
+    logger.info('Dataset is balanced')
+    
+    X_su = X_su.to_numpy()
+    y_su = y_su.to_numpy()
+    
+    #X_train, X_test, y_train, y_test = train_test_split(X_su, y_su, test_size = 0.25, random_state=0)
+    
+    model = keras.Sequential()
+    
+    model.add(layers.LSTM(512,input_shape=(2560,1),return_sequences=False))
+    
+    model.add(layers.Dense(4096, activation='sigmoid'))
+    model.add(layers.Dense(2048, activation='sigmoid'))
+    model.add(layers.Dense(1024, activation='sigmoid'))
+    model.add(layers.Dense(512, activation='sigmoid'))
+    model.add(layers.Dense(256, activation='sigmoid'))
+    model.add(layers.Dense(128, activation='sigmoid'))
+    model.add(layers.Dense(32, activation='sigmoid'))
+    model.add(layers.Dense(16, activation='sigmoid'))
+    model.add(layers.Dense(8, activation='sigmoid'))
+    model.add(layers.Dense(3, activation='softmax'))
+    
+    model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+        filepath= 'model_LSTM_SMOTE-' + str(epochs) + "-" + str(batch_size) + '.h5',
+        save_weights_only=True,
+        monitor='val_accuracy',
+        mode='max',
+        save_best_only=True)
+      
+    model.compile(optimizer =tf.keras.optimizers.Adam(learning_rate=0.001), loss="categorical_crossentropy", metrics="accuracy")
+    logger.info('Model is compiled')
+    
+    y_train_new = np.zeros((y_su.shape[0],3))
+    for i in range(y_su.shape[0]):
+        y_train_new[i][y_su[i]] = 1
+    
+    X_su = X_su.reshape(X_su.shape[0],X_su.shape[1],1)
+    
+    
+    history = model.fit(X_su,y_train_new,epochs=epochs,batch_size = batch_size, validation_split=0.05, callbacks = [model_checkpoint_callback])
+    logger.info('Model is trained')
+    
+    filepath = "modelLSTM_SMOTE-" + str(epochs) + "-" + str(batch_size) + "-val_accuracy-" + str(max(history.history['val_accuracy'])) + ".png"
+    
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'val'], loc='upper left')
+    plt.savefig(filepath)
+    driveUpload(service, filepath)
+    
+    filepath = "modelLSTM_SMOTE-" + str(epochs) + "-" + str(batch_size) + "-val_loss-" + str(max(history.history['val_loss'])) + ".png"
+    
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'val'], loc='upper left')
+    plt.savefig(filepath)
+    driveUpload(service, filepath)
+    
+    def inttostring(x):
+        if(x < 10):
+            return "0" + str(x)
+        else:
+            return str(x)
         
-#print(len(items))
-#print(items)
         
-i = 0
-while(i < len(items)):
-    try: 
-        request_file = service.files().get_media(fileId=items[i]['id'])
-        file = io.BytesIO()
-        downloader = MediaIoBaseDownload(file, request_file)
-        done = False
-        while done is False:
-            status, done = downloader.next_chunk()
-            #print(F'Download {int(status.progress() * 100)}.')
-    except HttpError as error:
-        print(F'An error occurred: {error}')
-
-    file_retrieved: str = file.getvalue()
-    with open(f"{items[i]['name']}", 'wb') as f:
-        f.write(file_retrieved)
-    df_temp = pd.read_csv(items[i]['name'], header=None)
-
-    name_type = 0 if items[i]['name'].find("CNT") != -1 else 1 if items[i]['name'].find("oh") != -1 else 2
-    df_temp.insert(0, "Type", [name_type for _ in range(df_temp.shape[0])])
-
-    df = pd.concat([df, df_temp])
-
-    df_temp = pd.DataFrame(None)
-
-    os.remove(items[i]['name'])
+    def floattostring(x):
+        s = str(x)
+        if(len(s) > 6):
+            s = s[:6]
+        elif len(s) < 6:
+            while(len(s) < 6):
+                s += "0"
+        return(s)
     
-    #print(i)
-
-    i += 1
-
-logger.info('Loaded dataset from Google Drive')
+    filepath = "modelLSTM_SMOTE." + inttostring(history.history['val_accuracy'].index(max(history.history['val_accuracy'])) + 1) + "-"
+    filepath = 'model_LSTM_SMOTE-' + str(epochs) + "-" + str(batch_size) + '.h5'
+    model.load_weights(filepath)
+    driveUpload(service, filepath)
     
-"""
-
-
-df = pd.read_csv("combined_data.csv", index_col=0)
-logger.info('Loaded dataset')
-
-
-y = df['Type']
-X = df.loc[:, df.columns != "Type"]
-
-
-su = BorderlineSMOTE(random_state=42)
-X_su, y_su = su.fit_resample(X, y)
-logger.info('Dataset is balanced')
-
-
-X_su = X_su.to_numpy()
-y_su = y_su.to_numpy()
-
-#X_train, X_test, y_train, y_test = train_test_split(X_su, y_su, test_size = 0.25, random_state=0)
-
-model = keras.Sequential()
-
-model.add(layers.LSTM(512,input_shape=(2560,1),return_sequences=False))
-
-model.add(layers.Dense(4096, kernel_initializer='normal',activation='linear'))
-model.add(layers.Dense(2048, kernel_initializer='normal',activation='linear'))
-model.add(layers.Dense(1024, kernel_initializer='normal',activation='linear'))
-model.add(layers.Dense(512, kernel_initializer='normal',activation='linear'))
-model.add(layers.Dense(256, kernel_initializer='normal',activation='linear'))
-model.add(layers.Dense(128, kernel_initializer='normal',activation='linear'))
-model.add(layers.Dense(32, kernel_initializer='normal',activation='linear'))
-model.add(layers.Dense(16, kernel_initializer='normal',activation='linear'))
-model.add(layers.Dense(8, kernel_initializer='normal',activation='linear'))
-model.add(layers.Dense(3, kernel_initializer='normal',activation='softmax'))
-
-#model.summary()
-
-
-model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
-    filepath= 'modelLSTM_SMOTE.{epoch:02d}-{accuracy:.4f}-{val_accuracy:.4f}.h5',
-    save_weights_only=True,
-    monitor='val_accuracy',
-    mode='max',
-    save_best_only=True)
-
-
-model.compile(loss='categorical_crossentropy',optimizer =tf.keras.optimizers.Adam(learning_rate=0.001),metrics=['accuracy'])
-logger.info('Model is compiled')
-
-
-y_train_new = np.zeros((y_su.shape[0],3))
-for i in range(y_su.shape[0]):
-    y_train_new[i][y_su[i]] = 1
-
-
-history = model.fit(X_su,y_train_new,epochs=5,validation_split=0.05,verbose=0, callbacks = [model_checkpoint_callback])
-logger.info('Model is trained')
-
-
-filepath = "modelLSTM_SMOTE-" + "val_accuracy-" + str(max(history.history['val_accuracy'])) + ".png"
-
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train', 'val'], loc='upper left')
-plt.savefig(filepath)
-
-
-filepath = "modelLSTM_SMOTE-" + "val_loss-" + str(max(history.history['val_loss'])) + ".png"
-
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'val'], loc='upper left')
-plt.savefig(filepath)
-
-def inttostring(x):
-    if(x < 10):
-        return "0" + str(x)
-    else:
-        return str(x)
+    y_pred = model.predict(X_su)
+    y_pred_new = np.zeros(y_pred.shape[0])
+    for i in range(y_pred.shape[0]):
+        y_pred_new[i] = np.argmax(y_pred[i])
     
     
-def floattostring(x):
-    s = str(x)
-    if(len(s) > 6):
-        s = s[:6]
-    elif len(s) < 6:
-        while(len(s) < 6):
-            s += "0"
-    return(s)
+    filepath = "modelLSTM_SMOTE_confusion_matrix-" + str(epochs) + "-" + str(batch_size) + "-val_accuracy-" + str(max(history.history['val_accuracy'])) + ".png"
+    
+    matrix = confusion_matrix(y_su, y_pred_new)
+    
+    fig, ax = plt.subplots(figsize=(7.5, 7.5))
+    ax.matshow(matrix, cmap=plt.cm.Blues, alpha=0.3)
+    for i in range(matrix.shape[0]):
+        for j in range(matrix.shape[1]):
+            ax.text(x=j, y=i,s=matrix[i, j], va='center', ha='center', size='xx-large')
+     
+    plt.xlabel('Predictions', fontsize=18)
+    plt.ylabel('Actuals', fontsize=18)
+    plt.title('Confusion Matrix', fontsize=18)
+    plt.savefig(filepath)
+    driveUpload(service, filepath)
 
+def main(argv):
+    # Default parameters
+    epochs = 3
+    batch_size = 64
+    noRows = 500
+    
+    try:
+       opts, args = getopt.getopt(argv,"e:, b:, n:",["ifile="])
+    except getopt.GetoptError:
+       print('LSTM_SMOTE.py -e <no epochs (default 3)> -b <batch size (default 64) > -n <number of rows to load from combined_data.csv> (default 500)')
+       sys.exit(2)
+    for opt, arg in opts:
+       if opt == '-h':
+          print('LSTM_SMOTE.py -e <no epochs (default 3)> -b <batch size (default 64) > -n <number of rows to load from combined_data.csv> (default 500, None for all rows)')
+          sys.exit()
+       elif opt in ("-e"):
+          epochs = int(arg)       
+       elif opt in ("-b"):
+          batch_size = int(arg)       
+       elif opt in ("-n"):
+           if arg == 'None':
+               noRows = None
+           else:
+               noRows = int(arg)                 
+    createModel(epochs, batch_size, noRows)
 
-filepath = "modelLSTM_SMOTE." + inttostring(history.history['val_accuracy'].index(max(history.history['val_accuracy'])) + 1) + "-"
-filepath = filepath + floattostring(max(history.history['accuracy'])) + "-" + floattostring(max(history.history['val_accuracy'])) + ".h5"
-model.load_weights(filepath)
-
-
-y_pred = model.predict(X_su)
-y_pred_new = np.zeros(y_pred.shape[0])
-for i in range(y_pred.shape[0]):
-    y_pred_new[i] = np.argmax(y_pred[i])
-
-
-filepath = "modelLSTM_SMOTE_confusion_matrix" + "val_accuracy-" + str(max(history.history['val_accuracy'])) + ".png"
-
-matrix = confusion_matrix(y_su, y_pred_new)
-
-fig, ax = plt.subplots(figsize=(7.5, 7.5))
-ax.matshow(matrix, cmap=plt.cm.Blues, alpha=0.3)
-for i in range(matrix.shape[0]):
-    for j in range(matrix.shape[1]):
-        ax.text(x=j, y=i,s=matrix[i, j], va='center', ha='center', size='xx-large')
- 
-plt.xlabel('Predictions', fontsize=18)
-plt.ylabel('Actuals', fontsize=18)
-plt.title('Confusion Matrix', fontsize=18)
-plt.savefig(filepath)
+if __name__ == '__main__':
+    main(sys.argv[1:])
